f14a519971661ddae9def6833322fd7496ed74f1
smooth rythm <rythmsmooth@gmail.com>
2025-08-08 18:49:52 +0700

    change button icon

--- src/components/TemplateMessages.tsx
+++ src/components/TemplateMessages.tsx
@@ -6,9 +6,12 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 
 // ---------------------- Import types, constants, avatar and icons ----------
-import { Clock, Send} from "lucide-react";
+import { Clock, Send } from "lucide-react";
 import { TemplateMessage, TemplateMessagesProps } from "@/types";
 import { templates, timeOptions, quickTimeOptions } from "@/constant";
+
+import requestButton from '@/assets/request.png';
+import sendButton from '@/assets/send.png';
 
 //============================================================================
 
@@ -67,7 +70,7 @@ export function TemplateMessages({ onSendMessage }: TemplateMessagesProps) {
       </div>
 
       <h3 className="text-base font-semibold text-foreground mb-2">Driver Request Templates</h3>
-      
+
       {/* Compact Template Message Buttons */}
       <div className="flex-1 space-y-2 overflow-y-auto ">
         <div className="space-y-2 w-full">
@@ -82,7 +85,7 @@ export function TemplateMessages({ onSendMessage }: TemplateMessagesProps) {
                   <Button
                     variant="template"
                     className="w-full justify-start gap-3 px-4 py-2 h-auto min-h-[60px] flex-col items-start bg-primary "
-                    >
+                  >
                     <div className="flex items-center gap-3 w-full">
                       <Icon size={22} />
                       <div className="flex-1 text-left">
@@ -93,7 +96,12 @@ export function TemplateMessages({ onSendMessage }: TemplateMessagesProps) {
                           {template.description}
                         </div>
                       </div>
-                      <Send size={16} className="opacity-60" />
+                      {/* <Send size={16} className="opacity-60" /> */}
+                      <img
+                        src={requestButton}
+                        alt="requestButton"
+                        className="w-[16px] h-[16px] rounded-full border-2 border-primary-border"
+                      />
                     </div>
                   </Button>
                 </AlertDialogTrigger>
