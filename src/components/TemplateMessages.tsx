1579c2cfdeafe0f2c4f7da5a8327a3354b8121cf
smooth rythm <rythmsmooth@gmail.com>
2025-08-08 18:54:00 +0700

    change button icon size

--- src/components/TemplateMessages.tsx
+++ src/components/TemplateMessages.tsx
@@ -100,7 +100,7 @@ export function TemplateMessages({ onSendMessage }: TemplateMessagesProps) {
                       <img
                         src={requestButton}
                         alt="requestButton"
-                        className="w-[16px] h-[16px] rounded-full border-2 border-primary-border"
+                        className="w-[50px] h-[50px] rounded-full border-2 border-primary-border"
                       />
                     </div>
                   </Button>
